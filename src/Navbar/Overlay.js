f8bc2b098b1402559bed5b6419f639bf9f606e46
Sean-Tosello <154922497+Sean-Tosello@users.noreply.github.com>
2024-02-22 15:07:24 -0800

    Updates

--- src/Navbar/Overlay.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Navbar.module.css';
-
-function Overlay({ show, text }) {
-  return <div style={{ display: show ? 'flex' : 'none', ...styles.Overlay }}>{text}</div>;
-}
-
-export default Overlay;
