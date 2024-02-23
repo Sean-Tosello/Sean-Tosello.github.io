ffb9e099ecaa4107d4269bbb24c28b87fe798868
Sean-Tosello <154922497+Sean-Tosello@users.noreply.github.com>
2024-02-23 11:00:11 -0800

    Revert "Updates"
    This reverts commit f8bc2b098b1402559bed5b6419f639bf9f606e46.

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './App.css';
+import Navbar from '../Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+import NavbarButton from './NavbarButton';
+import AboutOverlay from '../AboutOverlay/AboutOverlay';
+import ProjectsOverlay from '../ProjectsOverlay/ProjectsOverlay';
+import Resume from './SeanToselloResume.pdf';
+
+
+function Navbar() {
+  const openPDF = () => {
+    const pdfPath = Resume;
+    window.open(pdfPath, '_blank');
+  };
+  
+  return (
+    <nav>
+      <NavbarButton buttonText='About' overlayText={<AboutOverlay />} />
+      <NavbarButton buttonText='Projects' overlayText={<ProjectsOverlay />} />
+      <button onClick={openPDF}>Resume</button>
+    </nav>
+  )
+}
+
+export default Navbar;
