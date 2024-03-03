d332a5926346a317a4b5a1e68bd1f114a58bbbc5
Sean-Tosello <154922497+Sean-Tosello@users.noreply.github.com>
2024-03-02 18:16:14 -0800

    Consistent styling for optimized spacing across all viewports. Trimmed some unnecessary styling

--- src/Navbar/ProjectsOverlay/ProjectsOverlay.js
+++ src/Navbar/ProjectsOverlay/ProjectsOverlay.js
@@ -24,35 +24,25 @@ const ProjectsOverlay = () => {
     <div className={styles.projectsOverlay}>
       {/* Project Button Container */}
       <ul className={styles.buttonsList}>
-        <li className={styles.button}>
-          <div className={styles.buttonBox}>
-            <button className={styles.projectButton} onClick={() => toggleHandler(0)}><img className={styles.buttonFront} src={profileImg} alt="profile"/></button>
-            <h3>Personal Website</h3>
-          </div>
+        <li className={styles.buttonBox}>
+          <button className={styles.projectButton} onClick={() => toggleHandler(0)}><img className={styles.buttonFront} src={profileImg} alt="profile"/></button>
+          <h3>Personal Website</h3>
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.projectButton} onClick={() => toggleHandler(1)}><img className={styles.buttonFront} src={ravenousImg} alt="ravenous"/></button>
-            <h3>ravenous</h3> 
-          </div> 
+        <li className={styles.buttonBox}>
+          <button className={styles.projectButton} onClick={() => toggleHandler(1)}><img className={styles.buttonFront} src={ravenousImg} alt="ravenous"/></button>
+          <h3>ravenous</h3> 
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.projectButton} onClick={() => toggleHandler(2)}><img className={styles.buttonFront} src={colmarImg} alt="Colmar Academy"/></button>
-            <h3>Colmar Academy</h3>
-          </div>
+        <li className={styles.buttonBox}>
+          <button className={styles.projectButton} onClick={() => toggleHandler(2)}><img className={styles.buttonFront} src={colmarImg} alt="Colmar Academy"/></button>
+          <h3>Colmar Academy</h3>
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.projectButton} onClick={() => toggleHandler(3)}><img className={styles.buttonFront} src={teaImg} alt="Tea Cozy"/></button>
-            <h3>Tea Cozy</h3>
-          </div>
+        <li className={styles.buttonBox}>
+          <button className={styles.projectButton} onClick={() => toggleHandler(3)}><img className={styles.buttonFront} src={teaImg} alt="Tea Cozy"/></button>
+          <h3>Tea Cozy</h3>
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.projectButton} onClick={() => toggleHandler(4)}><img className={styles.buttonFront} src={excursionImg} alt="Excursion"/></button>
-            <h3>Excursion</h3>
-          </div> 
+        <li className={styles.buttonBox}>
+          <button className={styles.projectButton} onClick={() => toggleHandler(4)}><img className={styles.buttonFront} src={excursionImg} alt="Excursion"/></button>
+          <h3>Excursion</h3>
         </li>
       </ul>
       {/* Project Text Container */}
@@ -64,24 +54,24 @@ const ProjectsOverlay = () => {
           </div>
           <ul className={styles.featuresList}>
             <li><h4>Features:</h4></li>
-            <li><p> - Languages: JavaScript, HTML, CSS</p></li>
-            <li><p> - React Library</p></li>
-            <li><p> - React Components</p></li>
-            <li><p> - Original Custom Design</p></li>
+            <li><p> ~ Languages: JavaScript, HTML, CSS</p></li>
+            <li><p> ~ React Library</p></li>
+            <li><p> ~ React Components</p></li>
+            <li><p> ~ Original Custom Design</p></li>
           </ul>
         </div>
         <div className={styles.projectText} style={{ display: showProjectText[1] ? 'flex' : 'none'}}>
           <div className={styles.projectDescription}>
             <h4>Project Description:</h4>
-            <p>This website was created using React, Javascript, HTML and CSS. It is a single page application that showcases my skills and projects.</p>
+            <p>This project asked students to build a React-based web application called "ravenous" that gives food recommendations using react hooks to Yelp's API.</p>
           </div>
           <ul className={styles.featuresList}> 
             <li><h4>Features:</h4></li>
-            <li><p> - Languages: JavaScript, HTML, CSS</p></li>
-            <li><p> - React Library</p></li>
-            <li><p> - React Components</p></li>
-            <li><p> - Original Custom Design</p></li>
-            <li><p> - React Hook with Yelp API</p></li>
+            <li><p> ~ Languages: JavaScript, HTML, CSS</p></li>
+            <li><p> ~ React Library</p></li>
+            <li><p> ~ React Components</p></li>
+            <li><p> ~ Original Custom Design</p></li>
+            <li><p> ~ React Hook with Yelp API</p></li>
           </ul>
         </div> 
         <div className={styles.projectText} style={{ display: showProjectText[2] ? 'flex' : 'none'}}>
@@ -91,9 +81,9 @@ const ProjectsOverlay = () => {
           </div>
           <ul className={styles.featuresList}> 
             <li><h4>Features:</h4></li>
-            <li><p> - Languages: HTML, CSS</p></li>
-            <li><p> - Responsive Design</p></li>
-            <li><p> - Original Custom Design</p></li>
+            <li><p> ~ Languages: HTML, CSS</p></li>
+            <li><p> ~ Responsive Design</p></li>
+            <li><p> ~ Original Custom Design</p></li>
           </ul>
         </div>
         <div className={styles.projectText} style={{ display: showProjectText[3] ? 'flex' : 'none'}}>
@@ -103,8 +93,8 @@ const ProjectsOverlay = () => {
           </div>
           <ul className={styles.featuresList}> 
             <li><h4>Features:</h4></li>
-            <li><p> - Languages: HTML, CSS</p></li>
-            <li><p> - Original Custom Design</p></li>
+            <li><p> ~ Languages: HTML, CSS</p></li>
+            <li><p> ~ Original Custom Design</p></li>
           </ul>
         </div>
         <div className={styles.projectText} style={{ display: showProjectText[4] ? 'flex' : 'none'}}> 
@@ -114,7 +104,7 @@ const ProjectsOverlay = () => {
           </div>
           <ul className={styles.featuresList}> 
             <li><h4>Features:</h4></li>
-            <li><p> - Languages: HTML, CSS</p></li>
+            <li><p> ~ Languages: HTML, CSS</p></li>
           </ul>
         </div>   
       </div>
@@ -123,104 +113,3 @@ const ProjectsOverlay = () => {
 }
 
 export default ProjectsOverlay;
-  
-  
-//   return (
-//     <div className={styles.projectsOverlay}>
-//       <ul className={styles.buttonsList}>
-//         <li className={styles.buttonWithText}>
-//           <div className={styles.buttonBox}>
-//             <button className={styles.projectButton} onClick={() => toggleHandler(0)}><img className={styles.buttonFront} src={profileImg} alt="profile"/></button>
-//             <h3>Personal Website</h3>
-//           </div>
-//           <div className={styles.projectText} style={{ display: showProjectText[0] ? 'flex' : 'none'}}>
-//             <div className={styles.projectDescription}>
-//               <h4>Project Description:</h4>
-//               <p>This website was created using React, Javascript, HTML and CSS. It is a single page application that showcases my skills and projects.</p>
-//             </div>
-//             <ul className={styles.featuresList}>
-//               <li><h4>Features:</h4></li>
-//               <li><p> - Languages: JavaScript, HTML, CSS</p></li>
-//               <li><p> - React Library</p></li>
-//               <li><p> - React Components</p></li>
-//               <li><p> - Original Custom Design</p></li>
-//             </ul>
-//           </div> 
-//         </li>
-//         <li className={styles.buttonWithText}>
-//           <div className={styles.buttonBox}>
-//             <button className={styles.projectButton} onClick={() => toggleHandler(1)}><img className={styles.buttonFront} src={ravenousImg} alt="ravenous"/></button>
-//             <h3>ravenous</h3> 
-//           </div>
-//           <div className={styles.projectText} style={{ display: showProjectText[1] ? 'flex' : 'none'}}>
-//             <div className={styles.projectDescription}>
-//               <h4>Project Description:</h4>
-//               <p>This website was created using React, Javascript, HTML and CSS. It is a single page application that showcases my skills and projects.</p>
-//             </div>
-//             <ul className={styles.featuresList}> 
-//               <li><h4>Features:</h4></li>
-//               <li><p> - Languages: JavaScript, HTML, CSS</p></li>
-//               <li><p> - React Library</p></li>
-//               <li><p> - React Components</p></li>
-//               <li><p> - Original Custom Design</p></li>
-//               <li><p> - React Hook with Yelp API</p></li>
-//             </ul>
-//           </div> 
-//         </li>
-//         <li className={styles.buttonWithText}>
-//           <div className={styles.buttonBox}>
-//             <button className={styles.projectButton} onClick={() => toggleHandler(2)}><img className={styles.buttonFront} src={colmarImg} alt="Colmar Academy"/></button>
-//             <h3>Colmar Academy</h3>
-//           </div>
-//           <div className={styles.projectText} style={{ display: showProjectText[2] ? 'flex' : 'none'}}>
-//             <div className={styles.projectDescription}>
-//               <h4>Project Description:</h4>
-//               <p>For the project, I was provided with a mockup and resources and was tasked with building a responsive webpage and publishing it.</p>
-//             </div>
-//             <ul className={styles.featuresList}> 
-//               <li><h4>Features:</h4></li>
-//               <li><p> - Languages: HTML, CSS</p></li>
-//               <li><p> - Responsive Design</p></li>
-//               <li><p> - Original Custom Design</p></li>
-//             </ul>
-//           </div> 
-//         </li>
-//         <li className={styles.buttonWithText}>
-//           <div className={styles.buttonBox}>
-//             <button className={styles.projectButton} onClick={() => toggleHandler(3)}><img className={styles.buttonFront} src={teaImg} alt="Tea Cozy"/></button>
-//             <h3>Tea Cozy</h3>
-//           </div>
-//           <div className={styles.projectText} style={{ display: showProjectText[3] ? 'flex' : 'none'}}>
-//             <div className={styles.projectDescription}>
-//               <h4>Project Description:</h4>
-//               <p>For the project, I was provided with a mockup and resources and was tasked with building a responsive webpage.</p>
-//             </div>
-//             <ul className={styles.featuresList}> 
-//               <li><h4>Features:</h4></li>
-//               <li><p> - Languages: HTML, CSS</p></li>
-//               <li><p> - Original Custom Design</p></li>
-//             </ul>
-//           </div> 
-//         </li>
-//         <li className={styles.buttonWithText}>
-//           <div className={styles.buttonBox}>
-//             <button className={styles.projectButton} onClick={() => toggleHandler(4)}><img className={styles.buttonFront} src={excursionImg} alt="Excursion"/></button>
-//             <h3>Excursion</h3>
-//           </div>
-//           <div className={styles.projectText} style={{ display: showProjectText[4] ? 'flex' : 'none'}}> 
-//             <div className={styles.projectDescription}>
-//               <h4>Project Description:</h4>
-//               <p>For the project, I was provided with a mockup and resources and was tasked with building a responsive webpage.</p>
-//             </div>
-//             <ul className={styles.featuresList}> 
-//               <li><h4>Features:</h4></li>
-//               <li><p> - Languages: HTML, CSS</p></li>
-//             </ul>
-//           </div>  
-//         </li>
-//       </ul>
-//     </div>  
-//   );
-// }
-
-// export default ProjectsOverlay;
--- src/Navbar/SkillsetOverlay/SkillsetOverlay.js
+++ src/Navbar/SkillsetOverlay/SkillsetOverlay.js
@@ -16,82 +16,159 @@ const SkillsetOverlay = () => {
   
   return (
     <div className={styles.skillsetOverlay}>
+      {/* Skill Button Container */} 
       <ul className={styles.buttonsList}>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.skillsetButton} onClick={() => toggleHandler(0)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/javascript--v1.png" alt="javascript"/></button>
-            <h3>JavaScript</h3>
-          </div>
-          <div className={styles.skillText} style={{ display: showSkillText[0] ? 'flex' : 'none'}}>
-            <h4>JavaScript Projects:</h4>
-            <ul className={styles.projectList}>
-              <li><p> ~ Personal Website</p></li>
-              <li><p> ~ ravenous</p></li>
-            </ul>
-          </div> 
+        <li className={styles.buttonBox}>
+          <button className={styles.skillsetButton} onClick={() => toggleHandler(0)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/javascript--v1.png" alt="javascript"/></button>
+          <h3>JavaScript</h3>
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.skillsetButton} onClick={() => toggleHandler(1)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/react-native.png" alt="React"/></button>
-            <h3>React</h3> 
-          </div>
-          <div className={styles.skillText} style={{ display: showSkillText[1] ? 'flex' : 'none'}}>
-            <h4>React Projects:</h4>
-            <ul className={styles.projectList}> 
-              <li><p> ~ Personal Website</p></li>
-              <li><p> ~ ravenous</p></li>
-            </ul>
-          </div> 
+        <li className={styles.buttonBox}>
+          <button className={styles.skillsetButton} onClick={() => toggleHandler(1)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/react-native.png" alt="React"/></button>
+          <h3>React</h3> 
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.skillsetButton} onClick={() => toggleHandler(2)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5"/></button>
-            <h3>HTML5</h3>
-          </div>
-          <div className={styles.skillText} style={{ display: showSkillText[2] ? 'flex' : 'none'}}>
-            <h4>HTML5 Projects:</h4>
-            <ul className={styles.projectList}> 
-              <li><p> ~ Personal Website</p></li>
-              <li><p> ~ ravenous</p></li>
-              <li><p> ~ Colmar Academy</p></li>
-              <li><p> ~ Tea Cozy</p></li>
-              <li><p> ~ Excursion</p></li>
-            </ul>
-          </div> 
+        <li className={styles.buttonBox}>
+          <button className={styles.skillsetButton} onClick={() => toggleHandler(2)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5"/></button>
+          <h3>HTML5</h3>
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.skillsetButton} onClick={() => toggleHandler(3)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/css3.png" alt="css3"/></button>
-            <h3>CSS</h3>
-          </div>
-          <div className={styles.skillText} style={{ display: showSkillText[3] ? 'flex' : 'none'}}>
-            <h4>CSS Projects:</h4>
-            <ul className={styles.projectList}> 
-              <li><p> ~ Personal Website</p></li>
-              <li><p> ~ ravenous</p></li>
-              <li><p> ~ Colmar Academy</p></li>
-              <li><p> ~ Tea Cozy</p></li>
-              <li><p> ~ Excursion</p></li>
-            </ul>
-          </div> 
+        <li className={styles.buttonBox}>
+          <button className={styles.skillsetButton} onClick={() => toggleHandler(3)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/css3.png" alt="css3"/></button>
+          <h3>CSS</h3>
         </li>
-        <li className={styles.buttonWithText}>
-          <div className={styles.buttonBox}>
-            <button className={styles.skillsetButton} onClick={() => toggleHandler(4)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/python--v1.png" alt="python--v1"/></button>
-            <h3>Python</h3>
-          </div>
-          <div className={styles.skillText} style={{ display: showSkillText[4] ? 'flex' : 'none'}}> 
-            <h4>Python Projects:</h4>
-            <ul className={styles.projectList}>
-              <li>
-                <p>Coming Soon...</p>
-              </li>
-            </ul>
-          </div>  
+        <li className={styles.buttonBox}>
+          <button className={styles.skillsetButton} onClick={() => toggleHandler(4)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/python--v1.png" alt="python--v1"/></button>
+          <h3>Python</h3> 
         </li>
       </ul>
+      {/* Skill Text Container */}
+      <div className={styles.skillTextContainer}>
+        <div className={styles.skillText} style={{ display: showSkillText[0] ? 'flex' : 'none'}}>
+          <h4>JavaScript Projects:</h4>
+          <ul className={styles.projectList}>
+            <li><p> ~ Personal Website</p></li>
+            <li><p> ~ ravenous</p></li>
+          </ul>
+        </div> 
+        <div className={styles.skillText} style={{ display: showSkillText[1] ? 'flex' : 'none'}}>
+          <h4>React Projects:</h4>
+          <ul className={styles.projectList}> 
+            <li><p> ~ Personal Website</p></li>
+            <li><p> ~ ravenous</p></li>
+          </ul>
+        </div>
+        <div className={styles.skillText} style={{ display: showSkillText[2] ? 'flex' : 'none'}}>
+          <h4>HTML5 Projects:</h4>
+          <ul className={styles.projectList}> 
+            <li><p> ~ Personal Website</p></li>
+            <li><p> ~ ravenous</p></li>
+            <li><p> ~ Colmar Academy</p></li>
+            <li><p> ~ Tea Cozy</p></li>
+            <li><p> ~ Excursion</p></li>
+          </ul>
+        </div> 
+        <div className={styles.skillText} style={{ display: showSkillText[3] ? 'flex' : 'none'}}>
+          <h4>CSS Projects:</h4>
+          <ul className={styles.projectList}> 
+            <li><p> ~ Personal Website</p></li>
+            <li><p> ~ ravenous</p></li>
+            <li><p> ~ Colmar Academy</p></li>
+            <li><p> ~ Tea Cozy</p></li>
+            <li><p> ~ Excursion</p></li>
+          </ul>
+        </div>
+        <div className={styles.skillText} style={{ display: showSkillText[4] ? 'flex' : 'none'}}> 
+          <h4>Python Projects:</h4>
+          <ul className={styles.projectList}>
+            <li>
+              <p>Coming Soon...</p>
+            </li>
+          </ul>
+        </div>   
+      </div>
     </div>  
   );
 }
 
-export default SkillsetOverlay;
+export default SkillsetOverlay;  
+  
+  
+//   return (
+//     <div className={styles.skillsetOverlay}>
+//       <ul className={styles.buttonsList}>
+//         <li className={styles.buttonWithText}>
+//           <div className={styles.buttonBox}>
+//             <button className={styles.skillsetButton} onClick={() => toggleHandler(0)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/javascript--v1.png" alt="javascript"/></button>
+//             <h3>JavaScript</h3>
+//           </div>
+//           <div className={styles.skillText} style={{ display: showSkillText[0] ? 'flex' : 'none'}}>
+//             <h4>JavaScript Projects:</h4>
+//             <ul className={styles.projectList}>
+//               <li><p> ~ Personal Website</p></li>
+//               <li><p> ~ ravenous</p></li>
+//             </ul>
+//           </div> 
+//         </li>
+//         <li className={styles.buttonWithText}>
+//           <div className={styles.buttonBox}>
+//             <button className={styles.skillsetButton} onClick={() => toggleHandler(1)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/react-native.png" alt="React"/></button>
+//             <h3>React</h3> 
+//           </div>
+//           <div className={styles.skillText} style={{ display: showSkillText[1] ? 'flex' : 'none'}}>
+//             <h4>React Projects:</h4>
+//             <ul className={styles.projectList}> 
+//               <li><p> ~ Personal Website</p></li>
+//               <li><p> ~ ravenous</p></li>
+//             </ul>
+//           </div> 
+//         </li>
+//         <li className={styles.buttonWithText}>
+//           <div className={styles.buttonBox}>
+//             <button className={styles.skillsetButton} onClick={() => toggleHandler(2)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/html-5--v1.png" alt="html-5"/></button>
+//             <h3>HTML5</h3>
+//           </div>
+//           <div className={styles.skillText} style={{ display: showSkillText[2] ? 'flex' : 'none'}}>
+//             <h4>HTML5 Projects:</h4>
+//             <ul className={styles.projectList}> 
+//               <li><p> ~ Personal Website</p></li>
+//               <li><p> ~ ravenous</p></li>
+//               <li><p> ~ Colmar Academy</p></li>
+//               <li><p> ~ Tea Cozy</p></li>
+//               <li><p> ~ Excursion</p></li>
+//             </ul>
+//           </div> 
+//         </li>
+//         <li className={styles.buttonWithText}>
+//           <div className={styles.buttonBox}>
+//             <button className={styles.skillsetButton} onClick={() => toggleHandler(3)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/css3.png" alt="css3"/></button>
+//             <h3>CSS</h3>
+//           </div>
+//           <div className={styles.skillText} style={{ display: showSkillText[3] ? 'flex' : 'none'}}>
+//             <h4>CSS Projects:</h4>
+//             <ul className={styles.projectList}> 
+//               <li><p> ~ Personal Website</p></li>
+//               <li><p> ~ ravenous</p></li>
+//               <li><p> ~ Colmar Academy</p></li>
+//               <li><p> ~ Tea Cozy</p></li>
+//               <li><p> ~ Excursion</p></li>
+//             </ul>
+//           </div> 
+//         </li>
+//         <li className={styles.buttonWithText}>
+//           <div className={styles.buttonBox}>
+//             <button className={styles.skillsetButton} onClick={() => toggleHandler(4)}><img className={styles.buttonFront} src="https://img.icons8.com/color/48/python--v1.png" alt="python--v1"/></button>
+//             <h3>Python</h3>
+//           </div>
+//           <div className={styles.skillText} style={{ display: showSkillText[4] ? 'flex' : 'none'}}> 
+//             <h4>Python Projects:</h4>
+//             <ul className={styles.projectList}>
+//               <li>
+//                 <p>Coming Soon...</p>
+//               </li>
+//             </ul>
+//           </div>  
+//         </li>
+//       </ul>
+//     </div>  
+//   );
+// }
+
+// export default SkillsetOverlay;
