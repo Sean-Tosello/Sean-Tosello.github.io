3d683b0de034540ecab18845c56617711f9136df
Sean-Tosello <154922497+Sean-Tosello@users.noreply.github.com>
2024-03-05 17:00:29 -0800

    fixed icon bar expanding on hover, shifted text-shadow to only on headers, changed background color to dark grey, ammended footer content, turned off load animation

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,17 +2,9 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  const iconUrl = "https://icons8.com/";
-
   return (
     <div className={styles.footer}>
-      <div className={styles.copyright}>
-        <p>© 2024 Sean Tosello</p>
-      </div>
-      <div className={styles.iconCopyright}>
-        <a href={iconUrl}  target="_blank" rel="noopener noreferrer"><p>Icons by: https://icons8.com/</p></a>
-      </div>
-      
+      <p>© 2024 Sean Tosello</p>    
     </div>
   )
 }
